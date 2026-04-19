a2609ead5789d4839108288c1638a07948dd3765
Sean Newman <sean@snewman.co>
2026-04-19 23:38:29 +0000

    Fix shellcheck warning in stats script

--- scripts/stats.sh
+++ scripts/stats.sh
@@ -3,5 +3,5 @@
 tmpf=$(mktemp)
 cat data/stream*.txt | grep "data: " | sed 's/^data: //' | jq -R 'fromjson? | .timestamp' | awk '{ rem = $1 % 60; minute = $1 - rem; print minute }' | sort > "${tmpf}"
 cat "${tmpf}" | uniq -c | awk '{ avg = int($1 / 60); print avg }' | sort -nr | uniq -c
-echo "Total lines: $(cat ${tmpf} | wc -l)"
+echo "Total lines: $(cat "${tmpf}" | wc -l)"
 rm "${tmpf}"
