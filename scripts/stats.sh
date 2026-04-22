5b34f85c3426080d7ce750b2b571512129893976
Sean Newman <sean@snewman.co>
2026-04-22 20:00:01 +0000

    Update stats script to process events.jsonl files

--- scripts/stats.sh
+++ scripts/stats.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-tmpf=$(mktemp)
-cat data/stream*.txt | grep "data: " | sed 's/^data: //' | jq -R 'fromjson? | .timestamp' | awk '{ rem = $1 % 60; minute = $1 - rem; print minute }' | sort > "${tmpf}"
-uniq -c "${tmpf}" | awk '{ avg = int($1 / 60); print avg }' | sort -nr | uniq -c
-echo "Total lines: $(wc -l "${tmpf}")"
-rm "${tmpf}"
+find data -type f -name 'events-*.jsonl' -print0 | parallel -0 jq -r '.timestamp' | awk '{ rem = $1 % 60; minute = $1 - rem; print minute }' | sort | uniq -c | awk '{ avg = int($1 / 60); print avg }' | sort -nr | uniq -c
