2a30ca730af0eb6ded46bc9e94670b5e027f54be
Sean Newman <sean@snewman.co>
2026-05-04 02:16:26 +0000

    Add request logging

--- src/ws-app/src/http/handlers/mod.rs
+++ src/ws-app/src/http/handlers/mod.rs
@@ -1,7 +1,10 @@
 use crate::http::server::AppState;
 use axum::Router;
 use axum::routing::{get, post};
+use tower::ServiceBuilder;
 use tower_http::services::ServeDir;
+use tower_http::trace::{DefaultMakeSpan, DefaultOnResponse, TraceLayer};
+use tracing::Level;
 
 mod health;
 mod index;
@@ -13,5 +16,13 @@ pub fn router(app_state: AppState) -> Router {
         .route("/health", get(health::health))
         .route("/ingest", post(ingest::ingest))
         .nest_service("/assets", ServeDir::new("assets"))
+        .layer(
+            ServiceBuilder::new().layer(
+                TraceLayer::new_for_http()
+                    .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
+                    .on_request(())
+                    .on_response(DefaultOnResponse::new().level(Level::INFO)),
+            ),
+        )
         .with_state(app_state)
 }
--- src/ws-app/src/lib.rs
+++ src/ws-app/src/lib.rs
@@ -4,10 +4,9 @@ pub mod views;
 
 pub use db::Pool as DbPool;
 
-use tracing_subscriber::fmt::format::FmtSpan;
-
 pub fn init_tracing() {
     tracing_subscriber::fmt::fmt()
-        .with_span_events(FmtSpan::CLOSE)
+        .with_target(false)
+        .compact()
         .init();
 }
